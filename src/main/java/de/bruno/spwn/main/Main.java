17e763c509b9b17f900917b41d0578c12710e2d1
Bruno <87519676+Not-Bruno@users.noreply.github.com>
2023-08-10 08:03:27 +0200

    Add files via upload

---
+++ src/main/java/de/bruno/spwn/main/Main.java
@@ -0,0 +1,23 @@
+package de.bruno.spwn.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public Main plugin;
+	
+	public void onEnable() {
+		plugin = this;
+		
+		
+		
+		
+	}
+	
+	
+	
+	public Main getPlugin() {
+		return plugin;
+	}
+	
+}
